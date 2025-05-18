6d9ce11a8057c20624cd88a8b39a73b01524dffd
Pololak <pololakhldm@gmail.com>
2025-05-18 12:40:28 +0300

    qghwieuhgoiwqhoiughkldajhg

--- src/main.cpp
+++ src/main.cpp
@@ -31,20 +31,48 @@ protected:
         bg->setPosition(winSize / 2.f);
         layer->addChild(bg, -2);
 
+        auto bbgOutline = extension::CCScale9Sprite::create("square02b_small.png");
+        bbgOutline->setScale(.55f);
+        bbgOutline->setColor({0,0,0});
+        bbgOutline->setContentSize({186.f, 42.f});
+        bbgOutline->setPosition(winSize / 2.f);
+        layer->addChild(bbgOutline);
+
+        auto bbg = extension::CCScale9Sprite::create("square02b_small.png");
+        bbg->setScale(.5f);
+        bbg->setColor({130,220,130});
+        bbg->setContentSize({200.f, 40.f});
+        bbg->setPosition(winSize / 2.f);
+        layer->addChild(bbg);
+
         m_bubble = CCSprite::createWithSpriteFrameName("gj_navDotBtn_on_001.png");
         m_bubble->setPosition(winSize / 2.f);
+        m_bubble->setColor({0,200,0});
         layer->addChild(m_bubble);
+
+        auto leftLine = CCSprite::createWithSpriteFrameName("persp_outline_02_001.png");
+        leftLine->setScale(.725f);
+        leftLine->setRotation(90.f);
+        leftLine->setColor({0,0,0});
+        leftLine->setPosition({winSize.width / 2.f - 14.f, winSize.height / 2.f});
+        layer->addChild(leftLine);
+
+        auto rightLine = CCSprite::createWithSpriteFrameName("persp_outline_02_001.png");
+        rightLine->setScale(.725f);
+        rightLine->setRotation(-90.f);
+        rightLine->setColor({0,0,0});
+        rightLine->setPosition({winSize.width / 2.f + 14.f, winSize.height / 2.f});
+        layer->addChild(rightLine);
 
         setTouchEnabled(true);
         setKeypadEnabled(true);
         setAccelerometerEnabled(true);
-        setAccelerometerInterval(30.);
 
         return true;
     }
 
     virtual void didAccelerate(CCAcceleration* pAccelerationValue) {
-        m_bubble->setPositionX((CCDirector::sharedDirector()->getWinSize().width / 2.f) + pAccelerationValue->x * 100);
+        m_bubble->setPositionX((CCDirector::sharedDirector()->getWinSize().width / 2.f) + pAccelerationValue->x * 50);
     }
 
 public:
